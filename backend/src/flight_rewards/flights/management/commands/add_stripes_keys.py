8b10938d951436af1b3984bc8c11d5e7d93a4bf2
vardanyan1 <vardanyaninbox@gmail.com>
2023-08-22 09:43:13 +0400

    modify key input command

--- backend/src/flight_rewards/flights/management/commands/add_stripes_keys.py
+++ backend/src/flight_rewards/flights/management/commands/add_stripes_keys.py
@@ -11,14 +11,14 @@ class Command(BaseCommand):
         live_secret_key = os.environ.get("STRIPE_LIVE_SECRET_KEY")
         test_secret_key = os.environ.get("STRIPE_TEST_SECRET_KEY")
 
-        if live_secret_key:
-            live_key, created_live = APIKey.objects.get_or_create_by_api_key(live_secret_key)
-            if created_live:
-                self.stdout.write(self.style.SUCCESS("Live Stripe API key added."))
-            else:
-                self.stdout.write(self.style.SUCCESS("Live Stripe API key already exists."))
-        else:
-            self.stdout.write(self.style.ERROR("STRIPE_LIVE_SECRET_KEY environment variable not found."))
+        # if live_secret_key:
+        #     live_key, created_live = APIKey.objects.get_or_create_by_api_key(live_secret_key)
+        #     if created_live:
+        #         self.stdout.write(self.style.SUCCESS("Live Stripe API key added."))
+        #     else:
+        #         self.stdout.write(self.style.SUCCESS("Live Stripe API key already exists."))
+        # else:
+        #     self.stdout.write(self.style.ERROR("STRIPE_LIVE_SECRET_KEY environment variable not found."))
 
         if test_secret_key:
             test_key, created_test = APIKey.objects.get_or_create_by_api_key(test_secret_key)
